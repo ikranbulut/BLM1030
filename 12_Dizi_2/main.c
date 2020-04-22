657e45a7e4ee356c6090c7ed5f5974d3c99bd1c0
Huseyin Yuce <huseyin@marmara.edu.tr>
2020-04-22 13:38:43 +0300

    Çok Boyutlu diziler

--- 12_Dizi_2/main.c
+++ 12_Dizi_2/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <strings.h>
 
 int main() {
     char c_dizi1[9] = {'K', 'a', 'r', 'a', 'k', 't', 'e', 'r', '\0'};
@@ -13,11 +14,38 @@ int main() {
     int i1, i2, i3;
 
 
-    for (int i1 = 0; i1 < 10; ++i1) {
+    for (i1 = 0; i1 < 10; ++i1) {
         i_dizi1[i1] = i1 * i1;
         printf("%d\n", i_dizi1[i1]);
     }
 
+    printf("\n%s - %s", c_dizi1, c_dizi2);
+
+    for (i1 = 0; i1 < 2; ++i1) {
+        for (i2 = 0; i2 < 3; ++i2) {
+            for (i3 = 0; i3 < 4; ++i3) {
+                i_dizi2[i1][i2][i3] = (i1 + 2) * (i2 + 2) * (i3 + 2);
+                printf("%d\n", i_dizi2[i1][i2][i3]);
+            }
+        }
+
+    }
+
+    strcpy(c_dizi3[0], "Karekter dizileri");
+//   strcpy(c_dizi3[1],"Katar dizileri");
+
+    for (i1 = 0; i1 < 19; i1++) {
+        c_dizi3[1][i1] = 'A';
+    }
+    c_dizi3[1][i1] = '\0';
+
+    printf("\n%s\n%s\n", c_dizi3[0], c_dizi3[1]);
+
+    for (i1 = 0; i1 < 5; ++i1) {
+        printf("%d\n", i_dizi3[i1]);
+    }
+
+    printf("%s", c_dizi4);
 
     return 0;
 }
