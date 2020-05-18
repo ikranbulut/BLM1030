3a42b99d36a0daa32a5d62ee1cead6350192cfc1
Huseyin Yuce <huseyin@marmara.edu.tr>
2020-05-18 11:36:41 +0300

    6 Ornek ve guncelleme

--- 14_isaretci_4/main.c
+++ 14_isaretci_4/main.c
@@ -4,6 +4,9 @@ double ortalama(int *p, int n);
 int main() {
     int dizi[5] = {10, 2, 3, 17, 50};
     double ort;
+    printf("Dizi değişkeninin ilk elamanının pointer adresi : %p",dizi);
+    printf("Dizi değişkeninin ilk elamanının pointer adresi : %p",dizi[0]);
+    printf("\n--------------------\n");
     /* pointera deger olarak diziyi atiyoruz */
     ort = ortalama(dizi, 5);
     /* dˆnd¸r¸len deger (return)  */
